708070fad182d8504be83f99df976f017a04dbf7
J.D <79759124+ryu091@users.noreply.github.com>
2022-11-10 12:16:57 +0400

    Add files via upload

---
+++ Test.js
@@ -0,0 +1,37 @@
+(function() {
+    var aa= document.getElementsByTagName("input");
+    for (var i =0; i < aa.length; i++){
+        if (aa[i].value == 'GAM01')
+            aa[i].checked = false;
+        if (aa[i].value == 'GAM02')
+            aa[i].checked = false;
+        if (aa[i].value == 'GAM03')
+            aa[i].checked = false;
+        if (aa[i].value == 'GAM04')
+            aa[i].checked = false;
+        if (aa[i].value == 'GAM05')
+            aa[i].checked = false;
+        if (aa[i].value == 'GAM06')
+            aa[i].checked = false;
+        if (aa[i].value == 'GAM07')
+            aa[i].checked = false;
+        if (aa[i].value == 'GAM08')
+            aa[i].checked = false;
+        if (aa[i].value == 'GAM09')
+            aa[i].checked = false;
+        if (aa[i].value == 'GAM10')
+            aa[i].checked = false;
+        if (aa[i].value == 'Gas-Area')
+            aa[i].checked = false;
+        if (aa[i].value == 'HVSWG')
+            aa[i].checked = false;
+        if (aa[i].value == 'Site')
+            aa[i].checked = false;
+        if (aa[i].value == 'Ehvtx')
+            aa[i].checked = false;
+        if (aa[i].value == 'Auxdg')
+            aa[i].checked = false;
+        if (aa[i].value == 'GAM01')
+            aa[i].checked = false;
+    }
+})()
