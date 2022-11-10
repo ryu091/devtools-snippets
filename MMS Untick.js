56c352cfc84f032da91a49ce28d19baed7a4f6ed
J.D <79759124+ryu091@users.noreply.github.com>
2022-11-10 14:00:52 +0400

    Update MMS Untick.js

--- MMS Untick.js
+++ MMS Untick.js
@@ -2,36 +2,36 @@
     var aa= document.getElementsByTagName("input");
     for (var i =0; i < aa.length; i++){
         if (aa[i].value == 'GAM01')
-            aa[i].checked = false;
+            aa[i].click();
         if (aa[i].value == 'GAM02')
-            aa[i].checked = false;
+            aa[i].click();
         if (aa[i].value == 'GAM03')
-            aa[i].checked = false;
+            aa[i].click();
         if (aa[i].value == 'GAM04')
-            aa[i].checked = false;
+            aa[i].click();
         if (aa[i].value == 'GAM05')
-            aa[i].checked = false;
+            aa[i].click();
         if (aa[i].value == 'GAM06')
-            aa[i].checked = false;
+            aa[i].click();
         if (aa[i].value == 'GAM07')
-            aa[i].checked = false;
+            aa[i].click();
         if (aa[i].value == 'GAM08')
-            aa[i].checked = false;
+            aa[i].click();
         if (aa[i].value == 'GAM09')
-            aa[i].checked = false;
+            aa[i].click();
         if (aa[i].value == 'GAM10')
-            aa[i].checked = false;
+            aa[i].click();
         if (aa[i].value == 'Gas-Area')
-            aa[i].checked = false;
+            aa[i].click();
         if (aa[i].value == 'HVSWG')
-            aa[i].checked = false;
+            aa[i].click();
         if (aa[i].value == 'Site')
-            aa[i].checked = false;
+            aa[i].click();
         if (aa[i].value == 'Ehvtx')
-            aa[i].checked = false;
+            aa[i].click();
         if (aa[i].value == 'Auxdg')
-            aa[i].checked = false;
-        if (aa[i].value == 'GAM01')
-            aa[i].checked = false;
+            aa[i].click();
+        if (aa[i].value == '6.3Tx')
+            aa[i].click();
     }
 })()
